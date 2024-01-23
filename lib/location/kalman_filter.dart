211ac13abcd4bef9610f60fa75a009d469d412c2
PrintMutlu <143734044+PrintMutlu@users.noreply.github.com>
2024-01-23 08:02:19 +0300

    some fixes

--- lib/location/kalman_filter.dart
+++ lib/location/kalman_filter.dart
@@ -1,11 +1,7 @@
 import 'package:fyp_new/location/position.dart';
 
 class KalmanFilter {
-  double _lastEstimateX = 0.0;
-  double _lastEstimateY = 0.0;
-  double _errorEstimate = 1.0;
-  double _errorMeasure = 0.085;
-  double _q = 0.075;
+  double _lastEstimateX = 0.0, _lastEstimateY = 0.0, _errorEstimate = 1.0, _errorMeasure = 0.085, _q = 0.075;
 
   KalmanFilter() {
     _lastEstimateX = 0.0;
@@ -16,15 +12,12 @@ class KalmanFilter {
   }
   Position update(Position measurement) {
     double kalmanGain = _errorEstimate / (_errorEstimate + _errorMeasure);
-
     _lastEstimateX = _lastEstimateX + kalmanGain * (measurement.x - _lastEstimateX);
     _lastEstimateY = _lastEstimateY + kalmanGain * (measurement.y - _lastEstimateY);
-
     _errorEstimate = (1.0 - kalmanGain) * _errorEstimate +
         (_q * (_lastEstimateX - measurement.x).abs());
     _errorMeasure = (1.0 - kalmanGain) * _errorMeasure +
         (_q * (_lastEstimateY - measurement.y).abs());
-
     return Position(_lastEstimateX, _lastEstimateY);
   }
 }
--- lib/provider/location_provider.dart
+++ lib/provider/location_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class LocationProvider extends ChangeNotifier {
-  double x = 50;
+  double x = 0;
   double y = 0;
 
   void updateLocation(double newX, double newY) {
